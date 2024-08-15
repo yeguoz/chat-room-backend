0b2e87ca887d66388e54d0f291fe9c32991d9ddc
ye-guo <aidjajd@163.com>
2024-08-15 19:35:31 +0800

    feat: 退出登录功能

--- src/main/java/icu/yeguo/chat/controller/UserController.java
+++ src/main/java/icu/yeguo/chat/controller/UserController.java
@@ -9,6 +9,7 @@ import icu.yeguo.chat.model.dto.user.RegisterRequest;
 import icu.yeguo.chat.model.vo.UserVO;
 import icu.yeguo.chat.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -52,5 +53,15 @@ public class UserController {
     @GetMapping("currentUser")
     public Result<UserVO> getCurrentUser(HttpServletRequest req) {
         return ResultUtils.success(userService.getCurrentUser(req));
+    }
+
+    /*
+     * 退出登录
+     * */
+    @PostMapping("logout")
+    public Result<Integer> logout(HttpServletRequest req) {
+        HttpSession session = req.getSession();
+        session.invalidate();
+        return ResultUtils.success(1);
     }
 }
