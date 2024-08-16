ebddf65ea3fcde49b95807cb5d5e508b3df7e08e
ye-guo <aidjajd@163.com>
2024-08-16 11:58:18 +0800

    refactor: 重命名

--- src/main/java/icu/yeguo/chat/model/vo/Combined/MessageANDUserVo.java
+++ src/main/java/icu/yeguo/chat/model/vo/Combined/MessageANDUserVo.java
@@ -1,21 +1,21 @@
 package icu.yeguo.chat.model.vo.Combined;
 
 import icu.yeguo.chat.model.entity.Message;
-import icu.yeguo.chat.model.vo.UserVO;
+import icu.yeguo.chat.model.vo.UserVo;
 import lombok.Data;
 
 import java.io.Serial;
 import java.io.Serializable;
 
 @Data
-public class MessageANDUserVO implements Serializable {
+public class MessageANDUserVo implements Serializable {
     @Serial
     private static final long serialVersionUID = 8306385341140544171L;
     private Message message;
-    private UserVO userVO;
+    private UserVo userVo;
 
-    public MessageANDUserVO(Message msg2, UserVO userVO) {
+    public MessageANDUserVo(Message msg2, UserVo userVo) {
         this.message = msg2;
-        this.userVO = userVO;
+        this.userVo = userVo;
     }
 }
