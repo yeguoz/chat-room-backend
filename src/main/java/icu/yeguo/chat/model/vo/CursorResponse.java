86ab3cf302d938549ad46c2d083ef0ff33c05d7c
ye-guo <aidjajd@163.com>
2024-08-16 11:58:01 +0800

    feat: 增加游标分页响应类

---
+++ src/main/java/icu/yeguo/chat/model/vo/CursorResponse.java
@@ -0,0 +1,18 @@
+package icu.yeguo.chat.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@AllArgsConstructor // 为所有字段生成构造函数
+public class CursorResponse<T> implements Serializable {
+    @Serial
+    private static final long serialVersionUID = -9191962396766181716L;
+    private Long pageSize;
+    private Long cursorId;
+    private List<T> records;
+}
