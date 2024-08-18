d71f2806454c2a751130378d104fe78c119977a7
ye-guo <aidjajd@163.com>
2024-08-19 01:43:22 +0800

    feat: 增加查询聊天室消息，查询聊天室用户

--- src/main/java/icu/yeguo/chat/controller/ChatRoomController.java
+++ src/main/java/icu/yeguo/chat/controller/ChatRoomController.java
@@ -5,11 +5,14 @@ import icu.yeguo.chat.common.ResultUtils;
 import icu.yeguo.chat.model.entity.GroupRoom;
 import icu.yeguo.chat.model.vo.Combined.MessageANDUserVo;
 import icu.yeguo.chat.model.vo.CursorResponse;
+import icu.yeguo.chat.model.vo.UserVo;
 import icu.yeguo.chat.service.GroupRoomService;
 import icu.yeguo.chat.service.MessageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @Slf4j
@@ -30,10 +33,27 @@ public class ChatRoomController {
         return ResultUtils.success(groupRoomService.getGlobalRoom());
     }
 
+    /**
+     * 查询聊天室消息
+     *
+     * @return Result<CursorResponse < MessageANDUserVo>>
+     * @param: roomId
+     * @param: pageSize
+     * @param: cursorId
+     */
     @GetMapping("messages")
     public Result<CursorResponse<MessageANDUserVo>> getMessages(@RequestParam("roomId") Long roomId,
                                                                 @RequestParam("pageSize") Long pageSize,
-                                                                @RequestParam("cursorId") Long cursorId) {
+                                                                @RequestParam(value = "cursorId", required = false)
+                                                                Long cursorId) {
         return ResultUtils.success(messageService.cursorQuery(roomId, pageSize, cursorId));
+    }
+
+    /**
+     * 查询聊天室用户
+     */
+    @GetMapping("groupUsers")
+    public Result<List<UserVo>> getGroupUsers(@RequestParam("roomId") Long roomId) {
+        return ResultUtils.success(groupRoomService.getGroupUsers(roomId));
     }
 }
